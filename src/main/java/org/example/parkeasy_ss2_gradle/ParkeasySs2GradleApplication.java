6bbfea7fce4dd5fff2bf4df95e79c4baf3e54ef1
AussieK <yallsung85@gmail.com>
2024-11-05 16:41:34 +0100

    init

---
+++ src/main/java/org/example/parkeasy_ss2_gradle/ParkeasySs2GradleApplication.java
@@ -0,0 +1,13 @@
+package org.example.parkeasy_ss2_gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkeasySs2GradleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkeasySs2GradleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/parkeasy_ss2_gradle/ParkeasySs2GradleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.parkeasy_ss2_gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkeasySs2GradleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
