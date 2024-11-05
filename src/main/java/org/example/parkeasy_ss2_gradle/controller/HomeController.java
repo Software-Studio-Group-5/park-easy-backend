42abcae17ec522b40fdaa4da4e2e514053c66c13
AussieK <yallsung85@gmail.com>
2024-11-05 21:12:49 +0100

    add Home controller

---
+++ src/main/java/org/example/parkeasy_ss2_gradle/controller/HomeController.java
@@ -0,0 +1,14 @@
+package org.example.parkeasy_ss2_gradle.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    // request to first page
+    @GetMapping("/")
+    public String index(){
+        return "index"; // to find index.html in template package
+    }
+
+}
