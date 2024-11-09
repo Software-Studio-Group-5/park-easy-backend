d39f912d9412e2eac52e12a62134140c10439bad
AussieK <yallsung85@gmail.com>
2024-11-10 00:45:02 +0100

    Memberservice created. save method . @param MemberDTO class

--- src/main/java/org/example/parkeasy_ss2_gradle/service/MemberService.java
+++ src/main/java/org/example/parkeasy_ss2_gradle/service/MemberService.java
@@ -1,9 +1,17 @@
 package org.example.parkeasy_ss2_gradle.service;
 
+import lombok.RequiredArgsConstructor;
+import org.example.parkeasy_ss2_gradle.dto.MemberDTO;
+import org.example.parkeasy_ss2_gradle.repository.MemberRepository;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class MemberService {
+    private final MemberRepository memberRepository;
+    public void save(MemberDTO memberDTO) {
+
+    }
 }
 
 
